e27aa001cc9c89ec080ae555c783a8630f5fd8b8
squall <scw941021@gmail.com>
2021-03-08 18:56:26 -0800

    cart page implemented

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,3 +1,22 @@
 $(function () {
-  $(".amount").select2({ minimumResultsForSearch: -1 });
+  function calcTotal() {
+    var pRows = $(".prow");
+    var total = 0;
+    for (var i = 0; i < pRows.length; i++) {
+      var row = pRows[i];
+      var price = parseFloat($(".price", row).text().replace("$", ""));
+      var amount = parseInt($(".quantity select", row).val());
+      total += price * amount;
+      $(".total", row).text("$" + (price * amount).toFixed(2));
+    }
+    $(".subtotal .value").text("$" + total.toFixed(2));
+  }
+
+  $(".quantity select").select2({ minimumResultsForSearch: -1 });
+  $(".size select").select2({ minimumResultsForSearch: -1 });
+  $(".color select").select2({ minimumResultsForSearch: -1 });
+
+  $(".quantity select").change(calcTotal);
+
+  calcTotal();
 });
