58493412a34442e455b8c6f505b4bfffc71c9624
squall <scw941021@gmail.com>
2021-03-08 22:14:17 -0800

    popup-4 basically done

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -2,4 +2,10 @@ $(function () {
   // popup 03
   $("#popup-3 .size select").select2({ minimumResultsForSearch: -1 });
   $("#popup-3 .color select").select2({ minimumResultsForSearch: -1 });
+  // popup 04
+  $("#popup-4 .offer .color select").select2({ minimumResultsForSearch: -1 });
+  $("#popup-4 .offer .size select").select2({ minimumResultsForSearch: -1 });
+  $(".btn-hide-alarm").click(function () {
+    $(this).parent().hide();
+  });
 });
