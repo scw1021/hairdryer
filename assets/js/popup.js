49f142fe76556c042ca8f1a0c035314c5512b0b4
squall <scw941021@gmail.com>
2021-03-09 18:24:38 -0800

    popup-5 implemented

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -2,10 +2,16 @@ $(function () {
   // popup 03
   $("#popup-3 .size select").select2({ minimumResultsForSearch: -1 });
   $("#popup-3 .color select").select2({ minimumResultsForSearch: -1 });
+
   // popup 04
   $("#popup-4 .offer .color select").select2({ minimumResultsForSearch: -1 });
   $("#popup-4 .offer .size select").select2({ minimumResultsForSearch: -1 });
   $(".btn-hide-alarm").click(function () {
     $(this).parent().remove();
+  });
+
+  // popup 05
+  $("#popup-5 .btn-close").click(function () {
+    $("#popup-5").remove();
   });
 });
